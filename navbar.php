4afdd54d812ea71cc2238ab39f892b5712ec454c
ELHMD <91431003+ELHMD@users.noreply.github.com>
2021-09-26 18:42:56 +0000

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 <div class="nav-bar w-nav" id="nav" data-animation="default" data-collapse="medium" data-contain="1" data-duration="400">
         <div class="w-container">   
             <a class="brand-link w-nav-brand w--current" href="index.php">
-                <img class="logo" src="logo.png" style="width: 5%">
+                <img class="logo" src="logo1.png" style="width: 5%">
             </a>
             
             <nav class="nav-menu w-nav-menu" role="navigation" style="transform: translateY(0px) translateX(0px);">
